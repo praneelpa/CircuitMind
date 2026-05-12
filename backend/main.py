cca70326a860b5d206d8ef7969326a4ccf0c40af
praneelpa <praneel.code@gmail.com>
2026-05-11 18:14:39 -0700

    Site live

--- backend/main.py
+++ backend/main.py
@@ -5,7 +5,10 @@ from routes import ai
 app = FastAPI(title="CircuitMind API")
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "http://localhost:5173",
+        "https://circuit-mind-seven.vercel.app" 
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
